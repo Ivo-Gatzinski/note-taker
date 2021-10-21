f37519993f55619841149e2306895b909bd80fba
Ezhko <ucsd.camp.21@gmail.com>
2021-10-21 15:17:18 -0700

    add uuid

--- routes/html.js
+++ routes/html.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const {readAndAppend, readFromFile} = require("../helpers/fsUtils");
 const router = express.Router();
 
