bfbdc48839147302015c7844d8bb6a72ad822844
Ezhko <ucsd.camp.21@gmail.com>
2021-10-21 21:20:16 -0700

    fix the fixes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const api = require("./routes/index.js");
 
 const apiRouter = require("./api");
 const htmlRouter = require("./html");
@@ -9,8 +10,7 @@ router.use(apiRouter);
 
 const app = express();
 
-app.use("/html", htmlRouter);
-app.use("/api", apiRouter);
+app.use("/api", api);
 
 module.exports = app;
 module.exports = router;
