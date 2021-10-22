42ca546d1a15ce71f4e3c62d3e8e2258084ebfae
Ezhko <ucsd.camp.21@gmail.com>
2021-10-21 21:17:06 -0700

    commit

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,6 @@ const { v4: uuidv4 } = require("uuid");
 const {readAndAppend, readFromFile} = require("../helpers/fsUtils");
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
-const api = () => {
-
->>>>>>> parent of 7250295 (try to fix routing errors)
 router.get("/api/notes", (req, res) => {
     readFromFile(path.join(__dirname, "../db/db.json"), "utf-8")
       .then((data) => {
@@ -21,11 +16,7 @@ router.get("/api/notes", (req, res) => {
   });
 
   router.post("/api/notes", (req, res) => {
-<<<<<<< HEAD
 
-=======
-      
->>>>>>> parent of 7250295 (try to fix routing errors)
     const { id, title, text } = req.body;
   
     if (id && title && text) {
@@ -48,5 +39,4 @@ router.get("/api/notes", (req, res) => {
       res.json("Error in posting note");
     }
   });
-}
-  module.exports = api;
+  module.exports = router;
