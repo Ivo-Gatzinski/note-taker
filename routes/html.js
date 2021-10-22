46a238518060870d936ae683dbbb376f66c82bc7
Ezhko <ucsd.camp.21@gmail.com>
2021-10-22 09:39:37 -0700

    fix routing errors

--- routes/html.js
+++ routes/html.js
@@ -2,17 +2,13 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-const html = () => {
-
 router.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
 router.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
 
-}
-
-module.exports = html;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const api = require("./routes/index");
-
+const { clog } = require("./middleware/clog");
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+app.use(clog);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
 
 app.use(express.static("public"));
+
+app.use(api);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
